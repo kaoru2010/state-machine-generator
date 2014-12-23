afbc153f7af63cca852c071f083aa3358408df97
Ken Morishita <k_morishita@yumemi.co.jp>
2014-12-23 15:33:02 +0900

    fix swift parameter

--- gen.h
+++ gen.h
@@ -5,7 +5,7 @@
 #include <map>
 #include "smc_compiler.h"
 
-void gen_swift(std::string const& package_name, std::string const& fsmclass, state_map_list_t const& state_map_list, std::string const& start_map, std::string const& start_state, transition_set_t const& transition_set, std::string const& class_name, std::map<std::string, std::string> const& action_set, std::vector<std::string> const& include_list, std::vector<std::string> const& import_list);
+void gen_swift(std::string const& package_name, std::string const& fsmclass, state_map_list_t const& state_map_list, std::string const& start_map, std::string const& start_state, transition_set_t const& transition_set, std::string const& class_name, std::map<std::string, std::string> const& action_set, std::vector<std::string> const& include_list, std::vector<std::string> const& import_list, std::map<std::string, parameter_list_t> const& g_transition_params);
 void gen_javascript(std::string const& package_name, std::string const& fsmclass, state_map_list_t const& state_map_list, std::string const& start_map, std::string const& start_state, transition_set_t const& transition_set, std::string const& class_name);
 
 class intented_out
